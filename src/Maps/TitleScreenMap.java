95f8f0615268f124e2b42f0d09b1d5d01b698d2a
Taylor Lea <taylorlea@Taylors-MacBook-Air-7.local>
2022-10-19 09:37:47 -0400

    updates to title screen

--- src/Maps/TitleScreenMap.java
+++ src/Maps/TitleScreenMap.java
@@ -16,7 +16,7 @@ public class TitleScreenMap extends Map {
 
     private Sprite girl;
     private SpriteFont zooMania;
-    private Sprite elephantUncaged, girrafeUncaged, bunnyUncaged;
+    private Sprite elephantUncaged, girrafeUncaged, bunnyUncaged, lionUncaged;
 
     public TitleScreenMap() {
         super("title_screen_map.txt", new CommonTileset());
@@ -29,7 +29,7 @@ public class TitleScreenMap extends Map {
         zooMania = new SpriteFont("ZOO MANIA", 250, 100, "Impact", 62, new Color(0, 0, 0));
 
         //add elephant
-        Point elephantLocation = getMapTile(    9, 3).getLocation().subtractX(24).addY(6);
+        Point elephantLocation = getMapTile(    8, 3).getLocation().subtractX(24).addY(6);
         elephantUncaged = new Sprite(ImageLoader.load("elephantUncaged.png"));
         elephantUncaged.setScale(15);
         elephantUncaged.setLocation(elephantLocation.x, elephantLocation.y);
@@ -45,6 +45,12 @@ public class TitleScreenMap extends Map {
         bunnyUncaged = new Sprite(ImageLoader.load("BunnyUncaged.png"));
         bunnyUncaged.setScale(2);
         bunnyUncaged.setLocation(bunnyLocation.x, bunnyLocation.y);
+
+        //add lion
+        Point lionLocation = getMapTile(11, 6).getLocation();
+        lionUncaged = new Sprite(ImageLoader.load("LionUncaged.png"));
+        lionUncaged.setScale(9);
+        lionUncaged.setLocation(lionLocation.x, lionLocation.y);
     }
 
     @Override
@@ -55,6 +61,7 @@ public class TitleScreenMap extends Map {
         elephantUncaged.draw(graphicsHandler);
         girrafeUncaged.draw(graphicsHandler);
         bunnyUncaged.draw(graphicsHandler);
+        lionUncaged.draw(graphicsHandler);
     } 
 
 }

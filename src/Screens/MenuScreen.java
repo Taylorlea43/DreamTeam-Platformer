7c7d5713f9b56de5eaf480f65c8d322b6804e192
Hannah Pfersch <hannahpfersch@Hannahs-MacBook-Pro.local>
2022-11-28 14:13:11 -0500

    Menu items rearranged

--- src/Screens/MenuScreen.java
+++ src/Screens/MenuScreen.java
@@ -40,28 +40,26 @@ public class MenuScreen extends Screen {
 
 	@Override
 	public void initialize() { 
-		playGame = new SpriteFont("PLAY GAME", Config.GAME_WINDOW_WIDTH / 2 - 105, 150, "Comic Sans", 30,
-				new Color(49, 207, 240));
+		playGame = new SpriteFont("PLAY GAME", Config.GAME_WINDOW_WIDTH / 2 - 105, 150, "Comic Sans", 30, new Color(49, 207, 240));
 		playGame.setOutlineColor(Color.black);
 		playGame.setOutlineThickness(3);
 		
-		credits = new SpriteFont("CREDITS", Config.GAME_WINDOW_WIDTH / 2 - 105, 200, "Comic Sans", 30,
-				new Color(49, 207, 240));
-		credits.setOutlineColor(Color.black);
-		credits.setOutlineThickness(3);
+		instructions = new SpriteFont("INSTRUCTIONS", Config.GAME_WINDOW_WIDTH / 2 - 105, 200, "Comic Sans", 30, new Color(49, 207, 240));
+		instructions.setOutlineColor(Color.black);
+		instructions.setOutlineThickness(3);
 		
-		quit = new SpriteFont("QUIT", Config.GAME_WINDOW_WIDTH / 2 - 105, 250, "Comic Sans", 30,
-				new Color(49, 207, 240));
-		quit.setOutlineColor(Color.black);
-		quit.setOutlineThickness(3);
-		
-		avatar = new SpriteFont("CHOOSE AVATAR", Config.GAME_WINDOW_WIDTH / 2 - 105, 300, "Comic Sans", 30, new Color(49, 207, 240));
+		avatar = new SpriteFont("CHOOSE AVATAR", Config.GAME_WINDOW_WIDTH / 2 - 105, 250, "Comic Sans", 30, new Color(49, 207, 240));
 		avatar.setOutlineColor(Color.black);
 		avatar.setOutlineThickness(3);
 		
-		instructions = new SpriteFont("INSTRUCTIONS", Config.GAME_WINDOW_WIDTH / 2 - 105, 350, "Comic Sans", 30, new Color(49, 207, 240));
-		instructions.setOutlineColor(Color.black);
-		instructions.setOutlineThickness(3);
+		credits = new SpriteFont("CREDITS", Config.GAME_WINDOW_WIDTH / 2 - 105, 300, "Comic Sans", 30, new Color(49, 207, 240));
+		credits.setOutlineColor(Color.black);
+		credits.setOutlineThickness(3);
+		
+		quit = new SpriteFont("QUIT", Config.GAME_WINDOW_WIDTH / 2 - 105, 350, "Comic Sans", 30, new Color(49, 207, 240));
+		quit.setOutlineColor(Color.black);
+		quit.setOutlineThickness(3);
+		
 		
 		background = new TitleScreenMap();
 		background.setAdjustCamera(false);
@@ -109,46 +107,46 @@ public class MenuScreen extends Screen {
 		// sets color of spritefont text based on which menu item is being hovered
 		if (currentMenuItemHovered == 0) {
 			playGame.setColor(new Color(255, 215, 0));
+			instructions.setColor(new Color(49, 207, 240));
+			avatar.setColor(new Color(49, 207, 240));
 			credits.setColor(new Color(49, 207, 240));
 			quit.setColor(new Color(49, 207, 240));
-			avatar.setColor(new Color(49, 207, 240));
-			instructions.setColor(new Color(49, 207, 240));
 
 			pointerLocationX = 495;
 			pointerLocationY = 125;
 		} else if (currentMenuItemHovered == 1) {
 			playGame.setColor(new Color(49, 207, 240));
-			credits.setColor(new Color(255, 215, 0));
-			quit.setColor(new Color(49, 207, 240));
+			instructions.setColor(new Color(255, 215, 0));
 			avatar.setColor(new Color(49, 207, 240));
-			instructions.setColor(new Color(49, 207, 240));
+			credits.setColor(new Color(49, 207, 240));
+			quit.setColor(new Color(49, 207, 240));
 
 			pointerLocationX = 495;
 			pointerLocationY = 175;
 		} else if (currentMenuItemHovered == 2) {
 			playGame.setColor(new Color(49, 207, 240));
-			credits.setColor(new Color(49, 207, 240));
-			quit.setColor(new Color(255, 215, 0));
-			avatar.setColor(new Color(49, 207, 240));
 			instructions.setColor(new Color(49, 207, 240));
+			avatar.setColor(new Color(255, 215, 0));
+			credits.setColor(new Color(49, 207, 240));
+			quit.setColor(new Color(49, 207, 240));
 			
 			pointerLocationX = 495;
 			pointerLocationY = 225;
 		} else if (currentMenuItemHovered == 3) {
 			playGame.setColor(new Color(49, 207, 240));
-			credits.setColor(new Color(49, 207, 240));
-			quit.setColor(new Color(49, 207, 240));
-			avatar.setColor(new Color(255, 215, 0));
 			instructions.setColor(new Color(49, 207, 240));
+			avatar.setColor(new Color(49, 207, 240));
+			credits.setColor(new Color(255, 215, 0));
+			quit.setColor(new Color(49, 207, 240));
 			
 			pointerLocationX = 495;
 			pointerLocationY = 275;
 		} else if (currentMenuItemHovered == 4) {
 			playGame.setColor(new Color(49, 207, 240));
-			credits.setColor(new Color(49, 207, 240));
-			quit.setColor(new Color(49, 207, 240));
+			instructions.setColor(new Color(49, 207, 240));
 			avatar.setColor(new Color(49, 207, 240));
-			instructions.setColor(new Color(255, 215, 0));
+			credits.setColor(new Color(49, 207, 240));
+			quit.setColor(new Color(255, 215, 0));
 			
 			pointerLocationX = 495;
 			pointerLocationY = 325;
@@ -182,13 +180,13 @@ public class MenuScreen extends Screen {
 				screenCoordinator.setGameState(GameState.LEVEL);
 
 			} else if (menuItemSelected == 1) {
-				screenCoordinator.setGameState(GameState.CREDITS);
-			} else if (menuItemSelected == 2) {
-				System.exit(0);
-			} else if (menuItemSelected == 3) {
-				screenCoordinator.setGameState(GameState.AVATAR);
-			} else if (menuItemSelected ==4) {
 				screenCoordinator.setGameState(GameState.INSTUCTIONS);
+			} else if (menuItemSelected == 2) {
+				screenCoordinator.setGameState(GameState.AVATAR);
+			} else if (menuItemSelected == 3) {
+				screenCoordinator.setGameState(GameState.CREDITS);
+			} else if (menuItemSelected ==4) {
+				System.exit(0);
 			}
 		}
 	}
